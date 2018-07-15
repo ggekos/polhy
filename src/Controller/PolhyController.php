f456667d6ca8a0eef4e3bae818bf797c17b3f029
Alexandre Colella <alexandrecolella@MacBook-Pro.local>
2018-07-15 19:45:09 +0200

    wip

---
+++ src/Controller/PolhyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Yaml\Yaml;
+
+class PolhyController extends Controller
+{
+    /**
+     * @Route("/", name="polhy")
+     */
+    public function index(KernelInterface $kernel)
+    {
+        $values = Yaml::parseFile($kernel->getProjectDir().'/public/polhy.yml');
+
+        return $this->render('polhy/index.html.twig', [
+            'values' => $values,
+        ]);
+    }
+}
